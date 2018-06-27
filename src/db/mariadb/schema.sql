f15032a6eee7807ee5c4b62712f1b24857d3ef4c
Serubin <Serubin@serubin.net>
2018-06-27 19:52:51 -0400

    Add functional database to mariadb

---
+++ src/db/mariadb/schema.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE IF NOT EXISTS accounts;
+
+CREATE TABLE IF NOT EXISTS `accounts`.`users` ( `id` INT(5) NOT NULL AUTO_INCREMENT ,  `username` VARCHAR(255) NOT NULL ,  `name` VARCHAR(512) NOT NULL ,  `email` VARCHAR(255) NOT NULL ,  `profile` LONGBLOB NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `accounts`.`sessions` ( `id` INT(5) NOT NULL AUTO_INCREMENT , `user_id` INT(5) NOT NULL , `session_id` VARCHAR(512) NOT NULL , `expiration` TIMESTAMP NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+
