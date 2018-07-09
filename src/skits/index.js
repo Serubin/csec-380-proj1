0b2a5ed126c9251141a82d440d6032c3c92d6c7c
Serubin <Serubin@serubin.net>
2018-07-09 00:03:14 -0400

    Add unit tests for skit-nodejs

--- src/skits/index.js
+++ src/skits/index.js
@@ -10,99 +10,122 @@ let els = null; // Populate on ping
 
 const INDEX = 'skitdata';
 const TYPE = 'skit';
+function server (testing) {
 
-/**
- * Dynamic elastic search connection
- */
-function createElasticConnection () {
-    console.log("Attempting connection")
-    els = new elasticsearch.Client({
-        host: 'elk:9200',
-        requestTimeout: Infinity,
-        keepAlive: true
-    });
+    if (typeof testing == "undefined")
+        testing = false;
 
-    els.ping({
-        requestTimeout: 2500,
-    }, (error) => {
-        console.log("Attempting connection to elastic search failed, retrying")
-        if (error)
-            setTimeout(createElasticConnection, 2500)
-    });
+    /**
+    * Dynamic elastic search connection
+    */
+    function createElasticConnection () {
 
-}
-createElasticConnection();
+        if (testing) {
+            function genPromise() {
+                return new Promise(function(resolve, reject) { reject("No els"); });
+            }
 
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+            els = {
+                search: function() { return genPromise(); },
+                index: function() { return genPromise(); },
+                delete: function() { return genPromise(); }
+            };
 
-app.get('/', (req, res) => {
-    res.sendFile('static/index.html');
-})
-
-/**
- * Get's all skits
- */
-app.get('/getskits', (req, res) => {
-
-    els.search({
-        index: INDEX,
-        type: TYPE
-    }).then((resp) => {
-        const hits = [];
-
-        for (hit of resp.hits.hits)
-            hits.push({id: hit._id, ...hit._source});
-
-        res.send({result: 200, total: resp.hits.total, data: hits });
-    }).catch((err) => res.status(500).send( {error: err} ));
-
-});
-
-/**
- * Add new skit
- * @param skit - skit content (max char 140)
- */
-app.post('/addskit', (req, res) => {
-
-    if(!req.body.skit)
-        return res.status(400).send({ error: "missing parameter: skit"});
-
-    if (req.body.skit.length > 140)
-        return res.status(400).send({ error: "skit is too long"});
-
-
-    els.index({
-        index: INDEX,
-        type: TYPE,
-        body: {
-            user: 0,
-            content: req.body.skit,
-            replies: [],
-            timestamp: new Date().toJSON()
+            return;
         }
-    }).then((resp) => {
-        res.send({result: 201, id: resp._id})
-    }).catch((err) => res.status(500).send( {error: err} ));
 
-});
+        console.log("Attempting connection")
+        els = new elasticsearch.Client({
+            host: 'elk:9200',
+            requestTimeout: Infinity,
+            keepAlive: true
+        });
 
-/**
- * Remove skit by id
- * @param id - skit id
- */
-app.delete('/removeskit', (req, res) => {
-    if(!req.body.id)
-        return res.status(400).send({ error: "missing parameter: id"});
+        els.ping({
+            requestTimeout: 2500,
+        }, (error) => {
+            console.log("Attempting connection to elastic search failed, retrying")
+            if (error)
+                setTimeout(createElasticConnection, 2500)
+        });
 
-    els.delete({
-        index: INDEX,
-        type: TYPE,
-        id: req.body.id
-    }).then((resp) => {
-        res.send({result: 204, id: req.body.id})
-    }).catch((err) => res.status(500).send( {error: err} ));
+    }
+    createElasticConnection();
 
-});
+    app.use(bodyParser.json()); // for parsing application/json
+    app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.listen(7000, () => console.log("Starting server on port 3000"))
+    app.get('/', (req, res) => {
+        res.send("Skit core api");
+    })
+
+    /**
+    * Get's all skits
+    */
+    app.get('/getskits', (req, res) => {
+
+        els.search({
+            index: INDEX,
+            type: TYPE
+        }).then((resp) => {
+            const hits = [];
+
+            for (hit of resp.hits.hits)
+                hits.push({id: hit._id, ...hit._source});
+
+            res.send({result: 200, total: resp.hits.total, data: hits });
+        }).catch((err) => res.status(500).send( {error: err} ));
+
+    });
+
+    /**
+    * Add new skit
+    * @param skit - skit content (max char 140)
+    */
+    app.post('/addskit', (req, res) => {
+
+        if(!req.body.skit)
+            return res.status(400).send({ error: "missing parameter: skit"});
+
+        if (req.body.skit.length > 140)
+            return res.status(400).send({ error: "skit is too long"});
+
+
+        els.index({
+            index: INDEX,
+            type: TYPE,
+            body: {
+                user: 0,
+                content: req.body.skit,
+                replies: [],
+                timestamp: new Date().toJSON()
+            }
+        }).then((resp) => {
+            res.send({result: 201, id: resp._id})
+        }).catch((err) => res.status(500).send( {error: err} ));
+
+    });
+
+    /**
+    * Remove skit by id
+    * @param id - skit id
+    */
+    app.delete('/removeskit', (req, res) => {
+        if(!req.body.id)
+            return res.status(400).send({ error: "missing parameter: id"});
+
+        els.delete({
+            index: INDEX,
+            type: TYPE,
+            id: req.body.id
+        }).then((resp) => {
+            res.send({result: 204, id: req.body.id})
+        }).catch((err) => res.status(500).send( {error: err} ));
+
+    });
+
+    app.listen(7000, () => console.log("Starting server on port 7000"))
+    return app;
+}
+
+module.exports = server;
---
+++ src/skits/spec/spec.js
@@ -0,0 +1,46 @@
+var request = require('supertest');
+
+describe('loading express', function () {
+    const serverFunc = require('../index');
+    let server = serverFunc(true);
+
+    it('responds to /', function testSlash(done) {
+    request(server)
+        .get('/')
+        .expect(200, done);
+    });
+    it('responds to /getskits', function testGetSkit(done) {
+        request(server)
+            .get('/getskits')
+            .expect(500, done);
+    });
+    it('responds to /addskit', function testGetSkit(done) {
+        request(server)
+            .post('/addskit')
+            .expect(400, done);
+    });
+
+    it('responds to /addskit with data', function testGetSkit(done) {
+        request(server)
+            .post('/addskit')
+            .send({ skit: 'A wild skit' })
+            .expect(500, done);
+    })
+    it('responds to /removeskit', function testGetSkit(done) {
+        request(server)
+            .delete('/removeskit')
+            .expect(400, done);
+    });
+
+    it('responds to /removeskit with data', function testGetSkit(done) {
+        request(server)
+            .delete('/removeskit')
+            .send({ id: 'anid' })
+            .expect(500, done);
+    })
+    it('404 everything else', function testPath(done) {
+        request(server)
+            .get('/foo/bar')
+            .expect(404, done);
+    });
+});
