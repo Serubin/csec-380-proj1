92a1738703577599a9f8dee3c56a34d61e317eaa
ShermanCalliope\DrX250 <DrX250@hotmail.com>
2018-07-10 21:24:09 -0400

    Debugging endpoints

--- src/follow/endpoints.py
+++ src/follow/endpoints.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 from flask_mysqldb import MySQL
 import requests
 import json
@@ -21,38 +22,40 @@ mysql = MySQL(app)
 def index():
 	return 'You shouldn\'t be here' 
 
-@app.route('/UserSearch')
+@app.route('/UserSearch',methods=['POST'])
 def userSearch():
-    value = app.Request().headers
+    value = request.args.get('search')
     cur = mysql.connection.cursor
-    cur.execute('''SELECT id, username FROM accounts.users WHERE username LIKE %%%s%%;''', (value.search))
+    cur.execute('''SELECT id, username FROM accounts.users WHERE username LIKE %%%s%%;''', (value))
     users = JSON.loads(cur.fetchall())
-	return users
+    return users
 
-@app.route('/FollowUser')
+@app.route('/FollowUser',methods=['POST'])
 def followUser():
-    value = app.Request().headers
-    r = requests.post('/api/v1/IsAuthenticated',value.Auth)
+    auth = request.headers
+    user = request.args.get('userId')
+    r = requests.post('/api/v1/IsAuthenticated',header = {'Authorization' : auth['Authentication']})
     r.json()
     if r.authenticated:
         cur = mysql.connection.cursor()
-        cur.execute('''INSERT into follows VALUES (%d,%d)''',r.user_id,value.user)
+        cur.execute('''INSERT into follows VALUES (%d,%d)''',r.user_id,user)
         return 'Done' 
     else:
         return 'Not Authenticated'
 
-@app.route('/UnfollowUser')
+@app.route('/UnfollowUser',methods=['POST'])
 def unfollowUser():
-    value = app.Request().headers
-    r = requests.post('/api/v1/IsAuthenticated',value.Auth)
+    auth = request.headers
+    user = request.args.get('userId')
+    r = requests.post('/api/v1/IsAuthenticated',header = {'Authorization' : auth['Authentication']})
     r.json()
     if r.authenticated:
         cur = mysql.connection.cursor()
-        cur.execute('''DELETE from follows WHERE followerid = %d AND followingid = %d''',r.user_id,value.user)
+        cur.execute('''DELETE from follows WHERE followerid = %d AND followingid = %d''',r.user_id,userId)
         return 'Done' 
     else:
         return 'Not Authenticated'
 
 
 if __name__ == "__main__":
-	app.run()
+	app.run(host='0.0.0.0',port='8000')
