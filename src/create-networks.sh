276ae3dafc431a5bcbe511ddd6958ac6c39dae37
ShermanCalliope\DrX250 <DrX250@hotmail.com>
2018-07-10 20:37:55 -0400

    Added follows into docker-compose

--- src/create-networks.sh
+++ src/create-networks.sh
@@ -1,4 +1,4 @@
-networks="datanet skitnet replynet authnet sitenet usernet"
+networks="datanet skitnet replynet authnet sitenet usernet follownet"
 
 for net in $networks; do
     docker network create $net
