7c41d595480dda6700f346ee4aa907e498961c49
ShermanCalliope\DrX250 <DrX250@hotmail.com>
2018-07-13 05:15:15 -0400

    Added CI for follows

---
+++ src/follow/tests.py
@@ -0,0 +1,19 @@
+import unittest
+import endpoints
+
+class tests(unittest.TestCase):
+    
+    def test_index(self):
+        self.assertEqual(index(),'You shouldn\'t be here')
+        
+    def test_userSearch(self):
+        self.assertEqual(userSearch(),'')
+        
+    def test_followUser(self):
+        self.assertEqual(followUser(),'Not Authenticated')
+        
+    def test_unfollowUser(self):
+        self.assertEqual(unfollowUser(),'Not Authenticated')
+        
+if __name__ == '__main__':
+    unittest.main()
