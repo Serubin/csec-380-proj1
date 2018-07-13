c657dc07518387ba649d39e95b027463ecd9b6ff
Serubin <Serubin@serubin.net>
2018-07-12 21:33:58 -0400

    Add login barebones

---
+++ src/site/src/components/Login.vue
@@ -0,0 +1,68 @@
+<template>
+    <div id="login">
+        <div id="login-box">
+            <p>Username</p>
+            <input type="text" name="username" id="username" v-model="username" /><br/>
+            <p>Password</p>
+            <input type="password" name="password" id="password" v-model="password" @keyup.enter="doLogin"/><br/>
+            <button id="login" @click="doLogin">Log in</button>
+        </div>
+    </div>
+</template>
+
+<script>
+import Skits from './Skits'
+
+export default {
+    name: 'Login',
+    mounted () {
+    },
+    data () {
+        return {
+            username: '',
+            password: ''
+        }
+    },
+    methods: {
+        doLogin () {
+            this.$http.post('/v1/Authenticate',
+                {username: this.username, password: this.password}).then(response => {
+                console.log(response)
+            }).catch(err => {
+                console.log(err)
+            })
+        }
+    },
+    components: {
+        Skits
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+    #login {
+        margin: 3em auto;
+        text-align: center;
+    }
+    p {
+        margin: 1em 0 0.5em 0;
+    }
+
+    input {
+        border-radius: .4rem;
+        box-shadow: none;
+        box-sizing: inherit;
+        appearance: none;
+        background-color: transparent;
+        border: 0.01em solid #d1d1d1;
+        outline: 0;
+        padding: .6em 1.0em;
+        transition: 0.3s border-color ease-in-out;
+    }
+
+    input:focus {
+        border-color: #999;
+    }
+</style>
--- src/site/src/router/index.js
+++ src/site/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
+import Login from '@/components/Login'
 import UserPage from '@/components/UserPage'
 
 Vue.use(Router)
@@ -11,6 +12,11 @@ export default new Router({
             path: '/',
             name: 'Home',
             component: Home
+        },
+        {
+            path: '/login',
+            name: 'Login',
+            component: Login
         },
         {
             path: '/user/:userId',
